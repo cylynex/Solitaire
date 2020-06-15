fd857456b52d19cf73ab143bab0eb6e2e4a2ef57
Santo Commarato <cylynex@gmail.com>
2020-06-15 09:08:45 -0600

    Board Setup

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Card {
+
+    public string cardName;
+    public int cardValue;
+    public string cardSuit;
+    public Sprite cardImage;
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class GameManager : MonoBehaviour {
+
+    // Cards Management
+    public List<Card> cards = new List<Card>();
+    public Card[] deckOfCards = new Card[52];
+    public GameObject[] cardHolders;
+    public GameObject drawDeck;
+
+   
+
+}
